21ef11dbd3fbf433b0e3f6a5a83f0379948bdd5f
魔都一只土拨鼠 <imgoogege@gmail.com>
2020-04-30 15:28:04 +0800

    add

---
+++ public/mom.go
@@ -0,0 +1,5 @@
+package public
+// swap x and y  in arr.
+func swap(x,y int,arr []int){
+	arr[x],arr[y]  = arr[y],arr[x]
+}
---
+++ sort/quick_sort.go
@@ -0,0 +1,36 @@
+package sort
+
+import (
+	"github.com/googege/gotools/public"
+)
+func QuickSort(arr []int)[]int{
+	quickSort(0,len(arr)-1,arr)
+	return arr
+}
+
+func quickSort(left,right int,arr  []int){
+	// stop
+	if left > right {
+		return
+	}
+	//process
+	ge := findPriIndex(left,right,arr)
+	// trill down
+	quickSort(left,ge-1,arr)
+	quickSort(ge+1,right,arr)
+}
+
+func findPriIndex(left,right int,arr []int)int{
+	pri := left
+	index := pri+1
+	for i := index;i <= right;i++ {
+		if arr[i] < arr[pri] {
+			public.swap(i,index,arr)
+			index++
+		}
+	}
+	public.swap(pri,index-1,arr)
+	return index -1
+}
+
+
