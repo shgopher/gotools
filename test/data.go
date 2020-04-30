921aafe945478610da18fa53b2533f1b5bf34e62
魔都一只土拨鼠 <imgoogege@gmail.com>
2020-05-01 04:58:37 +0800

    add

--- test/data.go
+++ test/data.go
@@ -6,7 +6,7 @@ func TestDatasSliceInt(n int) []int {
 	result := make([]int, 0, n)
 	r := public.NewRand()
 	for i := 10; i <= n*10; i++ {
-		result = append(result, r.Intn(20)+r.Intn(30))
+		result = append(result, r.Intn(1000)+r.Intn(1000))
 	}
 	return result
 }
