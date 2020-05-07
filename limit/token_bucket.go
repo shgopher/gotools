7b294cfcaacee06d71a6da7f1a4c69d3275779e8
魔都一只土拨鼠 <imgoogege@gmail.com>
2020-05-07 11:41:30 +0800

    add

--- limit/token_bucket.go
+++ limit/token_bucket.go
@@ -1,1 +1,27 @@
 package limit
+
+import (
+	"time"
+)
+
+type TokenBucket struct {
+	tokenPool chan bool
+}
+
+func NewTokenBucket(maxCount int64,interval time.Duration)*TokenBucket {
+	t := TokenBucket{}
+	t.tokenPool =  make(chan bool,maxCount)
+	go func() {
+		ti := time.NewTicker(time.Duration(interval.Nanoseconds()/(maxCount*1000*1000))* time.Millisecond)
+		for  {
+			select {
+			case <- ti.C:
+				<- t.tokenPool
+			}
+		}
+	}()
+	return &t
+}
+func(t *TokenBucket)Run(){
+	t.tokenPool<- true// when the chan <- true , the length = length - 1
+}
--- limit/token_bucket_test.go
+++ limit/token_bucket_test.go
@@ -1,1 +1,20 @@
 package limit
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestTokenBucket(t *testing.T) {
+	tb := NewTokenBucket(10,time.Minute)
+	for i := 0;i < 2;i++ {
+		go func() {
+			for {
+				tb.Run()
+				fmt.Println("runing")
+			}
+		}()
+	}
+	time.Sleep(time.Second*100)
+}
