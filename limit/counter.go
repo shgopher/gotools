04e536b398e07b6695f4c059b41410c4f75f800c
魔都一只土拨鼠 <imgoogege@gmail.com>
2020-05-07 09:20:09 +0800

    add

--- limit/counter.go
+++ limit/counter.go
@@ -1,1 +1,45 @@
 package limit
+
+import (
+	"sync"
+	"time"
+)
+
+type Counter struct {
+	interval time.Duration
+	maxCount int64
+	count    int64
+	lock     sync.Mutex
+	ticker   *time.Ticker
+}
+
+func NewConter(maxCount int64, interval time.Duration) *Counter {
+	c := &Counter{
+		interval: interval,
+		maxCount: maxCount,
+		ticker: time.NewTicker(interval),
+	}
+	go func() {
+		for {
+			select {
+			case <-c.ticker.C:
+				c.lock.Lock()
+				c.count = 0
+				c.lock.Unlock()
+			}
+		}
+	}()
+	return c
+}
+
+// counter run .
+func (c *Counter) Run() bool {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if c.count > c.maxCount {
+		return false
+	} else {
+		c.count++
+		return true
+	}
+}
--- limit/counter_test.go
+++ limit/counter_test.go
@@ -1,1 +1,25 @@
 package limit
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestConter(t *testing.T) {
+	c := NewConter(2, time.Second)
+	for i:= 0;i < 10;i++ {
+		go func() {
+			for {
+				if c.Run(){
+					time.Sleep(time.Second*1)
+					fmt.Println("runing")
+				}else {
+					time.Sleep(time.Second*2)
+					fmt.Println("waiting")
+				}
+			}
+		}()
+	}
+time.Sleep(time.Second*100)
+}
